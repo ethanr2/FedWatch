52d568513be08be31e430c4307c4aabb11c17758
ethanr2 <ethanrhmn@gmail.com>
2020-06-23 17:57:00 -0500

    added short script to check if the CSV is being updated

---
+++ csv_check.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 23 17:53:21 2020
+
+@author: ethan
+"""
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('data/cme_database.csv')
+print(df.tail())
